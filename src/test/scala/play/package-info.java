436249527b51b009f4791710c96cbaddad7cc1ec
Nick Vintila <nick@semanticbeeng.com>
2014-12-04 04:17:00 -0800

    Keep a reference to source of code

---
+++ src/test/scala/play/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Cloned from
+ * https://github.com/playframework/playframework/tree/31995c8980f2d7d26be9ac3e5dafa25ad2da2649/framework/src/play-json/src/test/scala/play/api/libs/json
+ */
+package play;
+
